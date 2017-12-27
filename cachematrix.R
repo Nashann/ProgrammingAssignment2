6cf68ef683bd69d12d4ca6225a5eb5b1d0b24d81
Nashann <abinashanand01@gmail.com>
2017-12-27 19:14:16 +0530

    #Nashann
    Committed the updated the scripts for validation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Assignment-I Peer Assignment -- Lexical Scoping
+# Author: Abinash Anand G
+# Created Date: 24/12/2017
+#*********************************************************************************
 
-## Write a short comment describing this function
-
+# Initialising the value of the x matrix. Values are initialised from 1 to 9 in a 3*3 matrix.
+x <- matrix(c(0,2,4,5,3,5,7,5,2),3,3)
+#makeCacheMatrix-- takes x as an input argument for execution.
 makeCacheMatrix <- function(x = matrix()) {
+  mtx <- NULL #Initialising the matrix as an empty matrix
+  # The get set functions get initialised and the values are updated.
+  set <- function(y) {
+    x <<- y
+    mtx <<- NULL
+  }
+  get <- function() x
+  set_matrix <- function(solve) mtx <<- mtx  #generic function solves the equation a %*% x = b for x, where b can be either a vector or a matrix.
+  get_matrix <- function() mtx  # returns the stored matirx values
+  list(set = set, get = get,
+       set_matrix = set_matrix,
+       get_matrix = get_matrix) # collects and returns all the info as a list 
 
 }
 
 
-## Write a short comment describing this function
+# The functions calculates and returns the inverse of the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # gets the cached values
+  mtx <- x$get_matrix()
+  #If the cached value is available it returns the values
+  if(!is.null(mtx)) {
+    message("getting cached data")
+    return(mtx)
+  }
+  #If there is no cached values then set of new calculation starts.
+  data <- x$get()
+  mtx <- solve(data, ...)
+  x$set_matrix(mtx)
+  mtx
 }
+
+x
+mymatrix <-cacheSolve(makeCacheMatrix(x))
+mymatrix
